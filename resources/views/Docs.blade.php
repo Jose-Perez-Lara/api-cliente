c5baa648955036ba1324570c2db6c46fee1e9ba5
Jose-Perez-Lara <jperlar802@g.educaand.es>
2025-01-31 18:01:26 +0100

    edit rutas docs y readme

--- resources/views/Docs.blade.php
+++ resources/views/Docs.blade.php
@@ -24,48 +24,48 @@
                 <li class="bg-white p-4 rounded-md shadow-md">
                     <h3 class="text-xl font-semibold">GET /usuarios</h3>
                     <p>Devuelve todos los usuarios</p>
-                    <code class="bg-gray-100 text-sm p-2 rounded-md">https://localhost:8000/usuarios</code>
+                    <code class="bg-gray-100 text-sm p-2 rounded-md">https://localhost:8000/api/v1/usuarios</code>
                     <h4 class="mt-2 font-semibold">Ejemplo de Petición:</h4>
                     <pre class="bg-gray-100 text-sm p-2 rounded-md">
-GET https://localhost:8000/usuarios</pre>
+GET https://localhost:8000/api/v1/usuarios</pre>
                 </li>
                 <li class="bg-white p-4 rounded-md shadow-md">
                     <h3 class="text-xl font-semibold">POST /usuarios</h3>
                     <p>Crea un usuario</p>
-                    <code class="bg-gray-100 text-sm p-2 rounded-md">https://localhost:8000/usuarios</code>
+                    <code class="bg-gray-100 text-sm p-2 rounded-md">https://localhost:8000/api/v1/usuarios</code>
                     <h4 class="mt-2 font-semibold">Ejemplo de Petición:</h4>
                     <pre class="bg-gray-100 text-sm p-2 rounded-md">
-POST https://localhost:8000/usuarios 
+POST https://localhost:8000/api/v1/usuarios 
 Body:'{"nombre": "Juan Perez", "email": "juan@example.com", "edad": 30}'</pre>
                 </li>
                 <li class="bg-white p-4 rounded-md shadow-md">
                     <h3 class="text-xl font-semibold">GET /usuarios/{id}</h3>
                     <p>Devuelve el usuario por ID.</p>
-                    <code class="bg-gray-100 text-sm p-2 rounded-md">https://localhost:8000/usuarios/{id}</code>
+                    <code class="bg-gray-100 text-sm p-2 rounded-md">https://localhost:8000/api/v1/usuarios/{id}</code>
                     <h4 class="mt-2 font-semibold">Ejemplo de Petición:</h4>
                     <pre class="bg-gray-100 text-sm p-2 rounded-md">
-GET https://localhost:8000/usuarios/1 </pre>
+GET https://localhost:8000/api/v1/usuarios/1 </pre>
                 </li>
                 <li class="bg-white p-4 rounded-md shadow-md">
                     <h3 class="text-xl font-semibold">PUT/PATCH /usuarios/{id}</h3>
                     <p>Actualiza la información del usuario por su ID.</p>
-                    <code class="bg-gray-100 text-sm p-2 rounded-md">https://localhost:8000/usuarios/{id}</code>
+                    <code class="bg-gray-100 text-sm p-2 rounded-md">https://localhost:8000/api/v1/usuarios/{id}</code>
                     <h4 class="mt-2 font-semibold">Ejemplo de Petición por PUT:</h4>
                     <pre class="bg-gray-100 text-sm p-2 rounded-md">
-PUT https://localhost:8000/usuarios/1 
+PUT https://localhost:8000/api/v1/usuarios/1 
 Body:'{"nombre": "Juan Pérez", "email": "juan.perez@example.com", "edad": 31}'</pre>
                     <h4 class="mt-2 font-semibold">Ejemplo de Petición por PATCH:</h4>
                     <pre class="bg-gray-100 text-sm p-2 rounded-md">
-PATCH https://localhost:8000/usuarios/1 
+PATCH https://localhost:8000/api/v1/usuarios/1 
 Body:'{"edad": 14}'</pre>
                 </li>
                 <li class="bg-white p-4 rounded-md shadow-md">
                     <h3 class="text-xl font-semibold">DELETE /usuarios/{id}</h3>
                     <p>Borra un usuario por ID.</p>
-                    <code class="bg-gray-100 text-sm p-2 rounded-md">https://localhost:8000/usuarios/{id}</code>
+                    <code class="bg-gray-100 text-sm p-2 rounded-md">https://localhost:8000/api/v1/usuarios/{id}</code>
                     <h4 class="mt-2 font-semibold">Ejemplo de Petición:</h4>
                     <pre class="bg-gray-100 text-sm p-2 rounded-md">
-DELETE https://localhost:8000/usuarios/1</pre>
+DELETE https://localhost:8000/api/v1/usuarios/1</pre>
                 </li>
             </ul>
         </section>
